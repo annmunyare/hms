94bd6a62d394c5b7edd772fb5b27ca328babf7c1
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-07-02 13:05:53 +0300

    character encoding error

--- resources/views/group/group.blade.php
+++ resources/views/group/group.blade.php
@@ -4,7 +4,7 @@
 	<head>
 		<!-- Required meta tags -->
 		<!-- <meta charset="utf-8"> -->
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name = "csrf-token" content="{{csrf_token()}}">
         <!-- Latest compiled and minified CSS -->
