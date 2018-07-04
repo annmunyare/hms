b20870f9877733a31455d27d8abd84f01716070c
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-07-04 16:40:57 +0300

    bypassing authentication

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -29,7 +29,7 @@ class GroupController extends Controller
             $message = $array[0]["message"];
         }
 
-        if(stripos($message, "bomb") != false)
+        if(stripos($message, "bomb") !==false)
         {
             $responseMessage = $name.", polite words, please.";
             $url = "https://prod-00.westeurope.logic.azure.com:443/workflows/e478fda5f8504500817eda9e885369ce/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1Az6ERehLD6wjSFlk72ECOf1nJnsxE77uawXOXcElV8";
